3ac2821e9a799139aacd65adc5b624ad0ae734a2
Hartwelg <43598755+Hartwelg@users.noreply.github.com>
2018-11-08 19:53:18 -0800

    ship constructors in ApplicationController.java

--- src/main/java/controllers/ApplicationController.java
+++ src/main/java/controllers/ApplicationController.java
@@ -1,8 +1,7 @@
 package controllers;
 
 import com.google.inject.Singleton;
-import cs361.battleships.models.Game;
-import cs361.battleships.models.Ship;
+import cs361.battleships.models.*;
 import ninja.Context;
 import ninja.Result;
 import ninja.Results;
@@ -21,7 +20,20 @@ public class ApplicationController {
 
     public Result placeShip(Context context, PlacementGameAction g) {
         Game game = g.getGame();
-        Ship ship = new Ship(g.getShipType());
+        //Ship ship = new Ship(g.getShipType());
+        if (g.getShipType() == "MINESWEEPER")
+        {
+            Ship ship = new Minesweeper();
+        }
+        else if (g.getShipType() == "DESTROYER")
+        {
+            Ship ship = new Destroyer();
+        }
+        else if (g.getShipType() == "BATTLESHIP")
+        {
+            Ship ship = new Battleship();
+        }
+        
         boolean result = game.placeShip(ship, g.getActionRow(), g.getActionColumn(), g.isVertical());
         if (result) {
             return Results.json().render(game);
